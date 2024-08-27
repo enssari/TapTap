4749bc8bc4708a13ae3739d1b159605754249c6b
Enes <terdodios@gmail.com>
2024-08-27 17:42:17 +0300

    Initial Commit

---
+++ TapTap/TapTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTapApp.swift
+//  TapTap
+//
+//  Created by Enes Sarı on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TapTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
